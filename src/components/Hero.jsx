e1ba96ae5b8d41a3aa70a2b8cc029e85292d9577
Marcinn88 <111724779+Marcinn88@users.noreply.github.com>
2024-07-07 01:41:21 +0200

    menu

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,15 +3,48 @@ import styles from "./Hero.module.css";
 import { MenuEl } from "./MenuEl";
 import { Nav } from "./Nav";
 import { OrderList } from "./OrderList";
+import data from "../JSON/menu.json";
+import { nanoid } from "nanoid";
+import { Link } from "react-router-dom";
+
+import list from "../images/list.svg";
+import waste from "../images/waste.svg";
+import fork from "../images/forklift.svg";
+import hammer from "../images/hammer.svg";
+import chart from "../images/chart.svg";
+import cart from "../images/cart.svg";
+import profile from "../images/profile.svg";
+import results from "../images/results.svg";
 
 export const Hero = ({ token }) => {
   return (
     <>
       <Nav />
       <div className={styles.Hero_wrapper}>
-        <MenuEl />
-        {/* <OrderList /> */}
-        {/* <Dashboard /> */}
+        <Link to="/precizo/orders/">
+          <MenuEl key={nanoid()} obraz={list} nazwa="Zlecenia" />
+        </Link>
+        <Link to="/precizo/">
+          <MenuEl key={nanoid()} obraz={waste} nazwa="Straty godzinowe" />
+        </Link>
+        <Link to="/precizo/">
+          <MenuEl key={nanoid()} obraz={fork} nazwa="Wezwij transport" />
+        </Link>
+        <Link to="/precizo/">
+          <MenuEl key={nanoid()} obraz={hammer} nazwa="Zgłoś awarię" />
+        </Link>
+        <Link to="/precizo/">
+          <MenuEl key={nanoid()} obraz={chart} nazwa="Statystyki" />
+        </Link>
+        <Link to="/precizo/">
+          <MenuEl key={nanoid()} obraz={cart} nazwa="Zamówienia" />
+        </Link>
+        <Link to="/precizo/">
+          <MenuEl key={nanoid()} obraz={profile} nazwa="Mój profil" />
+        </Link>
+        <Link to="/precizo/">
+          <MenuEl key={nanoid()} obraz={results} nazwa="Wyniki próbkowania" />
+        </Link>
       </div>
     </>
   );
--- src/components/MenuEl.jsx
+++ src/components/MenuEl.jsx
@@ -1,11 +1,11 @@
 import styles from "./MenuEl.module.css";
 
-export const MenuEl = ({ token }) => {
+export const MenuEl = ({ token, obraz, nazwa }) => {
   return (
     <>
       <div className={styles.MenuEl_wrapper}>
-        <img alt="" src="" />
-        <p>Zlecenia</p>
+        <img className={styles.MenuEl_img} alt="zlecenia" src={obraz} />
+        <p className={styles.MenuEl_title}>{nazwa}</p>
       </div>
     </>
   );
