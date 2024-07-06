e56b539413df4edc586a5a3f65de69ae928dc238
Marcinn88 <111724779+Marcinn88@users.noreply.github.com>
2024-07-06 16:11:44 +0200

    work

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,38 @@
+import { Routes, Route } from "react-router-dom";
+import { Dashboard } from "./components/Dashboard";
 import { Main } from "./components/Main";
+import { OrderList } from "./components/OrderList";
+import { Layout } from "./layouts/layout";
 
 const App = () => {
   console.log("server uruchomiony");
-  return <Main />;
+  const tokenChecker = () => {
+    try {
+      const token = localStorage.getItem("token");
+      const parsedToken = JSON.parse(token).token;
+      return parsedToken;
+    } catch (error) {
+      localStorage.setItem("token", JSON.stringify({ token: "" }));
+      const token = localStorage.getItem("token");
+      const parsedToken = JSON.parse(token).token;
+      return parsedToken;
+    }
+  };
+  return (
+    <Routes>
+      <Route path="/precizo/" element={<Layout token={tokenChecker()} />}>
+        <Route path="/precizo/" element={<Main token={tokenChecker()} />} />
+        <Route
+          path="/precizo/orders"
+          element={<OrderList token={tokenChecker()} />}
+        />
+        <Route
+          path="/precizo/dashboard"
+          element={<Dashboard token={tokenChecker()} />}
+        />
+        {/* <Route path="*" element={<NoMatch />} /> */}
+      </Route>
+    </Routes>
+  );
 };
 export default App;
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,10 +1,39 @@
 import styles from "./Nav.module.css";
 import menuIco from "../images/menu-burger.svg";
 import logoImg from "../images/Precizo.png";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
 export const Nav = ({ selected, token }) => {
+  const [navMenu, navMenuChange] = useState(false);
+
+  const ref = () => {
+    window.location.reload(false);
+  };
+  const openMenu = () => {
+    navMenuChange(true);
+  };
+
+  const closeMenu = () => {
+    navMenuChange(false);
+  };
   return (
     <>
       <div className={styles.mainNav}>
+        {navMenu && (
+          <div className={styles.mainNav_Shadowbox} onClick={closeMenu}></div>
+        )}
+        {navMenu && (
+          <ul className={styles.mainNav_menu}>
+            <li className={styles.mainNav_menuEl}>MENU</li>
+            <li className={styles.mainNav_menuEl}>
+              <Link to="/orders/">ZLECENIA</Link>
+            </li>
+            <li className={styles.mainNav_menuEl}>INFORMACJE</li>
+            <li className={styles.mainNav_menuEl}>POMOC</li>
+            <li className={styles.mainNav_menuEl}>WYLOGUJ</li>
+          </ul>
+        )}
         <img className={styles.mainNav_logoImg} src={logoImg} alt="logo" />
         <div className={styles.mainNav_rightSide}>
           <ul className={styles.mainNav_list}>
@@ -15,9 +44,9 @@ export const Nav = ({ selected, token }) => {
             className={styles.mainNav_menuIco}
             src={menuIco}
             alt="ikona menu"
+            onClick={openMenu}
           />
         </div>
-        {/* <h2 className={styles.mainNavHeader}>nawigacja</h2> */}
       </div>
     </>
   );
