1e9e2d59321a9927d096494c6122ab5c1d099d4f
Marcinn88 <111724779+Marcinn88@users.noreply.github.com>
2024-07-23 06:51:03 +0200

    FIX

--- src/components/Lab.jsx
+++ src/components/Lab.jsx
@@ -414,7 +414,7 @@ export const Lab = ({ token }) => {
                             </div>
                             <div className={styles.Lab_rowInfo_el}>
                               <p className={styles.Lab_order_infoTitle}>
-                                Sprawozdanie:
+                                Sprawozdanie ISIR:
                               </p>
                               {ISIR_required ? (
                                 <p className={styles.Lab_order_infoValue_red}>
